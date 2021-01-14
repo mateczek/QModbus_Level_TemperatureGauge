878a707e35a74cb65801b61ad563ba03e660e6e3
mateczek <marcof@interia.pl>
2021-01-15 00:28:18 +0100

    Update coreProg.cpp
    add coment

--- coreProg.cpp
+++ coreProg.cpp
@@ -5,9 +5,9 @@ coreProg::coreProg(QObject *parent) : QObject(parent)
 {
     mbus=new qmodbus(this);
     connect(mbus,&qmodbus::finished,this,[this](){
-       QTimer::singleShot(10000,mbus,SLOT(start()));
+       QTimer::singleShot(10000,mbus,SLOT(start()));//reconect after 10s after modbus thread finisch
     });
-    connect(mbus,SIGNAL(updateData(int,int, int)),this,SIGNAL(updateData(int, int, int)));
+    connect(mbus,SIGNAL(updateData(int,int, int)),this,SIGNAL(updateData(int, int, int))); //modbus data update emit signal. coreObj re_emit signal
 }
 
 coreProg::~coreProg()
