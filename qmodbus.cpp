07343d4799906feb560268175f4e4d322ecdc07d
mateczek <marcof@interia.pl>
2021-01-15 00:47:53 +0100

    Update qmodbus.cpp
    add comment

--- qmodbus.cpp
+++ qmodbus.cpp
@@ -8,35 +8,35 @@ void qmodbus::run(){
     QVector<quint16> tab(2);
     QModbusDataUnit du(QModbusDataUnit::HoldingRegisters,10,tab);
     QModbusReply *reply;
-    //odpowiedź modbusa
+    //define modbus reply function ....................................................................[1]
     auto rpl_redy =[&](){
         if(!reply->error()){
             emit updateData(id, reply->result().value(0),reply->result().value(1));
-            tim_event_timer.start(1000);
+            tim_event_timer.start(1000); //next modbus Request after one second
         }else{
             exit();
         }
         reply->deleteLater();
     };
-    //zdarzenie timera
+    //define timer timeout function........................................................................[2]
     auto timeout= [&](){
-        reply=mc.sendReadRequest(du,255);
+        reply=mc.sendReadRequest(du,255); //new Request modbus adress 255 
         if(reply->isFinished()){
-            delete reply;
+            delete reply;        
         }else{
-            connect(reply,&QModbusReply::finished,rpl_redy);
+            connect(reply,&QModbusReply::finished,rpl_redy); //set callback function when reply ready.  call[1]
         }
     };
-    //zmiana stanu urządzenia modbusa
+    //define modbus device state change function...........................................................[3] 
     auto stateChange = [&](QModbusClient::State state){
         if (state==QModbusClient::ConnectedState){
-            tim_event_timer.start(1000);
+            tim_event_timer.start(1000);                  
         }else if(state==QModbusClient::UnconnectedState) {
             exit();
         }
     };
-    connect(&tim_event_timer,&QTimer::timeout,timeout);
-    connect(&mc, &QModbusClient::stateChanged,stateChange);
+    connect(&tim_event_timer,&QTimer::timeout,timeout);           //set callback function ->[2] as timeut
+    connect(&mc, &QModbusClient::stateChanged,stateChange);       //set callback function ->[3] when qmodbus state change
     mc.setConnectionParameter(QModbusDevice::NetworkAddressParameter, "127.0.0.1");
     mc.setConnectionParameter(QModbusDevice::NetworkPortParameter,502);
     mc.setTimeout(500);
@@ -44,7 +44,7 @@ void qmodbus::run(){
         exit();
         return;
     }
-    exec();
+    exec(); //qt event loop 
 }
 
 qmodbus::qmodbus(QObject *parent) : QThread(parent)
